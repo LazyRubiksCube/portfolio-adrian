a4f6c09acd22212f06dc4b9ef1fbac4745ffe09e
Adrian Ayala <ayala.adrian@gmail.com>
2023-03-08 01:19:21 -0800

    added headed color and animated circles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles';
 
 type Props = {}
 
+// Introduction text along with several bits about me
 export default function Hero({}: Props) {
     const [text, count] = useTypewriter({
     words: [
-      "Hi! The Name's Adrian Ayala",
-      "Just a guy that loves coffee, reading, mountain biking, and sleeping",
+      "Hi! The name's Adrian Ayala.",
+      "Just a guy that loves coffee, reading, mountain biking, and sleeping.",
       "<ButLovesToLearnCoding />",
+      "Oh! I also love Warhammer 40K"
     ],
     loop: true,
-    delaySpeed: 2000,
-  })
+    delaySpeed: 4000,
+  });
+// 
   return (
     <div>
-      
+      <BackgroundCircles />
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor='F7AB0A'/>
+      </h1>  
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import Header from '../../components/Header'
 import Hero from '../../components/Hero'
+import type { NextPage } from 'next'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
       <Head>
         <title>adrianAYALA</title>
       </Head>     
@@ -29,6 +25,8 @@ export default function Home() {
       {/* Projects */}
 
       {/* Contact Me */}
-    </>
-  )
-}
+    </div>
+  );
+};
+
+export default Home;
