154c8cab9b18761cf3d8908b5cdfd7b3d76a5b70
Adrian Ayala <ayala.adrian@gmail.com>
2023-03-14 15:11:54 -0700

    flex stuff not working?

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -5,7 +5,9 @@ type Props = {}
 
 export default function ExperienceCard({}: Props) {
   return (
-   <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px]'>
+   <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px]
+   md:w-[600px] xl:w-[900px] snap-center p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'
+   >
     <motion.img
       initial={{
         y: -100,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,22 @@
-import Head from 'next/head';
-import Header from '../../components/Header';
-import Hero from '../../components/Hero';
-import About from '../../components/About';
-import type { NextPage } from 'next';
-import WorkExperience from '../../components/WorkExperience';
+import Head from "next/head";
+import Header from "../../components/Header";
+import Hero from "../../components/Hero";
+import About from "../../components/About";
+import type { NextPage } from "next";
+import WorkExperience from "../../components/WorkExperience";
+import Skills from "../../components/Skills";
 
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>adrianAYALA</title>
       </Head>     
       
       <Header />
       {/* Hero */}
-      <section id='hero' className='snap-start'>
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
@@ -30,6 +31,9 @@ const Home: NextPage = () => {
       </section>
       
       {/* Skills */}
+      <section id='skills' className='snap-start'>
+        <Skills />
+      </section>
 
       {/* Projects */}
 
